1c8bcb3fa9da6c4fb691a5230b99d3e427799d13
SunnyFall <sunnyfall@linux.myguest.virtualbox.org>
2022-12-17 16:33:36 +0300

    -h added

--- topsize.sh
+++ topsize.sh
@@ -4,15 +4,16 @@ N='all'
 s="0"
 minsize=1
 opt="0"
+dir_given='0'
 for arg in "$@" ; do
-	if [ $arg = "--" ] ;
+	if [ "$arg" = "--" ] ;
 	then
 		opt="1"
 	elif [ $opt = "0" ] ;
 	then 	
 		if [ $s = '1' ] ;
 		then	
-			if [[ $arg =~ ^[0-9]+$ ]];
+			if [[ "$arg" =~ ^[0-9]+$ ]];
 			then
 				minsize=$arg
 				s='0'
@@ -20,7 +21,7 @@ for arg in "$@" ; do
 				echo 'Error: wrong format for minsize'>&2
 				exit 1
 			fi
-		elif [ $arg = '--help' ] ;
+		elif [ "$arg" = '--help' ] ;
 		then
 			echo "topsize [--help] [-h] [-N] [-s minsize] [--] [dir...]"
 			echo " Выводит список N самых больших по размеру файлов из заданного каталога и всех его подкаталогов, размер которых превышает заданный размер minsize."
@@ -32,107 +33,75 @@ for arg in "$@" ; do
 			echo "-- - разделение опций и каталога (поддержка каталогов, начинающихся с минуса)"
 			exit 0		
 		
-		elif [ $arg = '-s' ] ;
+		elif [ "$arg" = '-s' ] ;
 		then
 			s='1'
-		elif [ $arg = "-h" ] ;
+		elif [ "$arg" = "-h" ] ;
 		then
 			h="1"
-		elif [[ $arg =~ ^-[0-9]+$ ]] ;
+		elif [[ "$arg" =~ ^-[0-9]+$ ]] ;
 		then
 			N=$arg
-		elif [ ${arg:0:1} = "-" ] ;
+		elif [ "${arg:0:1}" = "-" ] ;
 		then
 			echo "Error: no option $arg">&2
 			exit 1
+		else
+		dir_given=1
 		fi
+	else
+		dir_given=1
 	fi
 done
 if [ $s = 1 ] ;
-then
-echo 'Error: no minsize given'>&2
+then	
+	echo 'Error: no minsize given'>&2
 exit 1
 fi
-#echo 'minsize is '$minsize
-#echo 'N is '$N
-directs="0"
 opt="0"
-for arg in "$@" ; do
-	if [ $arg = "--" ] ;
-	then
-		opt="1"
-	elif [ $opt = "0" ] ;
-	then 	
-		if [ $s = '1' ] ;
-		then	
-			s='0'
-		elif [ $arg = '-s' ]; 
-		then
-			s='1'
-		elif ! [ ${arg:0:1} = "-" ] ;
-		then
-			new_dir=$arg
-			directs='1'
-			#echo $new_dir
-			if [ $h = '0' ];
-			then
-				if [ $N = 'all' ] ;
-				then 
-					find $new_dir -size +$minsize'c' -type f  -printf '%s %P\n' | sort -r -n
-				else
-					find $new_dir -size +$minsize'c' -type f -printf '%s %P\n'   | sort -r -n | head $N
-				fi
-			else
-				if [ $N = 'all' ] ;
-				then 
-					find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n 
-				else
-					find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n | head $N
-				fi	
-			fi
-		fi
-			
-	else
-		new_dir=$arg
-		directs='1'
-		#echo $new_dir
-		if [ $h = '0' ];
-		then
-			if [ $N = 'all' ] ;
-			then 
-				find $new_dir -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n
-			else
-				find $new_dir -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n | head $N
-			fi
-		else
-			if [ $N = 'all' ] ;
-			then 
-				find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n 
-			else
-				find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n | head $N
-			fi	
-		fi
-	fi
-done
-if [ $directs = "0" ] ;
+if [[ $dir_given = 0 ]] ;
 then
-	#echo $PWD
-	if [ $h = '0' ];
-	then
-		if [ $N = 'all' ] ;
-		then 
-			find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n 
-		else
-			find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n | head $N
-		fi	
-	else
-		if [ $N = 'all' ] ;
-		then 
-			find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n 
-			
-		else
-			find -size +$minsize'c' -type f -printf '%s %P\n'  | sort -r -n | head $N
+	find -size +$minsize"c" -type f 
+else
+	for arg in "$@" ; do
+		if [ "$arg" = "--" ] ;
+		then
+			opt="1"
+		elif [ $opt = "0" ] ;
+		then 	
+			if [ $s = '1' ] ;
+			then	
+				s='0'
+			elif [ "$arg" = '-s' ]; 
+			then
+				s='1'
+			elif ! [ "${arg:0:1}" = "-" ] ;
+			then
+					
+				find "$arg" -size +$minsize"c" -type f 		
+			fi
 				
-		fi	
+		else
+				
+			find "$arg" -size +$minsize"c" -type f 
+			
+		fi
+	done 
+fi | sort -u | while read name
+do
+
+	
+	if [ $h = 1 ] ;
+	then
+		du -bh "$name"
+	else
+		du -b "$name"	
 	fi
+done | sort -rh | if [ $N != 'all' ] ; then
+	head $N 
+else
+	cat
 fi
+
+
+	
